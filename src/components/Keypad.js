124cc8a9a1b66a972adc83645fb447e9256b9b14
Josh Wilde <jowilde11@gmail.com>
2024-12-09 12:25:23 -0500

    finsihed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
+function Keypad() {
+    const handleButtonClick = (value) => {
+        console.log(value);
+    };
+
+    const handleInputChange = () => {
+        console.log('Entering password...');
+    };
+
     return (
-        <div></div>
-    )
+        <div className="keypad">
+            <input type="password" onChange={handleInputChange} />
+            {[...Array(10).keys()].map(number => (
+                <button key={number} onClick={() => handleButtonClick(number)}>
+                    {number}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export default Keypad;
